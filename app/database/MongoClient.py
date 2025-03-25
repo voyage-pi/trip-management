41790f45890cb092adc425c56151d43d2a822363
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2025-03-25 13:56:09 +0000

    Connection made to the mongo-trip service

--- app/database/MongoClient.py
+++ app/database/MongoClient.py
@@ -1,27 +1,26 @@
-from pymongo import MongoClient as mongoDB
-from dotenv import load_dotenv,dotenv_values
+from app.schemas.trips_schema import TripList,Trip
+from pymongo import MongoClient 
+from dotenv import load_dotenv
 import os 
 
 load_dotenv()
 
 
-mongoHost= os.getenv("MONGO_HOST","mongo")
+mongoHost= os.getenv("MONGO_HOST","mongo-trip")
 mongoPort= os.getenv("MONGO_PORT",27017)
 mongoUser = os.getenv("MONGO_USER")
 mongoPwd = os.getenv("MONGO_PASSWORD")
-mongoDatabase = os.getenv("MONGO_DATABASE")
+mongoDatabase = os.getenv("MONGO_DATABASE","voyage-db")
 
-class MongoClient():
+class DBClient():
     def __init__(self):
-        self.client=mongoDB(f"mongodb://{mongoUser}:{mongoPwd}@{mongoHost}:{mongoPort}/")
-        self.current_database=None
-        self.current_collection=None
-    def use_db(self,db_name):
-        self.current_database=self.client[db_name]
-        return self
-    def use_collection(self,collection_name):
-        assert self.current_database != None
-        self.current_collection= self.current_database[collection_name]
-        return self
+        url=f"mongodb://{mongoUser}:{mongoPwd}@{mongoHost}:{mongoPort}/"
+        self.client=MongoClient(url)
+        self.db=self.client[str(mongoDatabase)]
+        self.collection=self.db["trips"]
 
+    def insert_trip(self,trip:TripList):
+        data=trip["trips"]
+        result=self.collection.insert_many(data)
+        return result 
 
