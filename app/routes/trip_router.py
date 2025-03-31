9dbfdf7467aa35c017461eb4faa58a2d5f1b9b45
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2025-03-31 19:49:16 +0100

    Making first request and generate an itinerary, with some bugs

--- app/routes/trip_router.py
+++ app/routes/trip_router.py
@@ -2,9 +2,9 @@ from app.database import MongoClient
 from app.schemas.response import ResponseBody
 from fastapi import APIRouter
 from app.schemas.trips_schema import Trip
-from app.schemas.forms_schema import Form
+from app.schemas.forms_schema import Form, TripType
 from app.database.MongoClient import DBClient
-from fastapi import HTTPException
+import requests as request  
 import json
 from datetime import datetime,timedelta
 
@@ -17,83 +17,35 @@ router = APIRouter(
 # Mock trips data
 @router.post("/trips",response_model=ResponseBody)
 async def trip_creation(forms:Form):
-    people=forms.people 
-    # here the request has to be made to the recommendation system since is what is going to make the trip structure
-    trip1: Trip = {
-        "image": "https://example.com/image1.jpg",
-        "days": [
-            {
-                "template_type": "leisure",
-                "places": [
-                    {
-                        "placeId": "place123",
-                        "location": ("Lisbon", "Portugal"),
-                        "start_time": datetime.now(),
-                        "end_time": datetime.now() + timedelta(hours=2),
-                    },
-                    {
-                        "placeId": "place456",
-                        "location": ("Porto", "Portugal"),
-                        "start_time": datetime.now() + timedelta(hours=3),
-                        "end_time": datetime.now() + timedelta(hours=5),
-                    },
-                ],
-                "routes": [
-                    {
-                        "polylines": ["abc123", "def456"],
-                        "duration": datetime.now() + timedelta(hours=1, minutes=30),
-                    }
-                ],
-            }
-        ],
-        "state":"incoming",
-        "people": ["user1", "user2"],
-    }
-
-    trip2: Trip = {
-        "image": "https://example.com/image2.jpg",
-        "days": [
-            {
-                "template_type": "business",
-                "places": [
-                    {
-                        "placeId": "place789",
-                        "location": ("Madrid", "Spain"),
-                        "start_time": datetime.now() + timedelta(days=1),
-                        "end_time": datetime.now() + timedelta(days=1, hours=4),
-                    }
-                ],
-                "routes": [
-                    {
-                        "polylines": ["ghi789", "jkl012"],
-                        "duration": datetime.now() + timedelta(hours=4),
-                    }
-                ],
-            },
-            {
-                "template_type": "sightseeing",
-                "places": [
-                    {
-                        "placeId": "place321",
-                        "location": ("Barcelona", "Spain"),
-                        "start_time": datetime.now() + timedelta(days=2),
-                        "end_time": datetime.now() + timedelta(days=2, hours=3),
-                    }
-                ],
-                "routes": [
-                    {
-                        "polylines": ["mno345", "pqr678"],
-                        "duration": datetime.now() + timedelta(hours=2, minutes=15),
-                    }
-                ],
-            },
-        ],
-        "state": "drafted",
-        "people": ["user3", "user4", "user5"]
-    }
-    client=DBClient()
-    ids=client.post_trip([trip1,trip2])
-    return ResponseBody({"ids":ids},"Trips created")
+    trip_type=forms.tripType   
+    place= forms.place
+    questions = []
+    for k,questions in forms.questions.items():
+        questions.extend([q for q in questions ])
+    delta = timedelta(days=forms.duration) 
+    requestBody={
+        "questionnaire":[q.dict() for q in questions],  # ou q.model_dump(),
+        "start_date":forms.dateStart,
+        "end_date":forms.dateStart + delta,
+        "budget":forms.budget
+    } 
+    if TripType(trip_type)==TripType.PLACE:
+        coordinates=place.coordinates 
+        requestBody["coordinates"]=coordinates.dict()
+        print(coordinates.dict())
+    elif TripType(trip_type)==TripType.ROAD:
+        origin=place.origin
+        destination=place.destination
+    elif TripType(trip_type)==TripType.ZONE:
+        coordinates=place.coordinates 
+    #generate the itinerary
+    requestBody['start_date'] = requestBody['start_date'].isoformat()
+    requestBody['end_date'] = requestBody['end_date'].isoformat()
+    response=request.post("http://recommendations:8080/trip",json=requestBody)
+    if response.status_code != 200:
+        return ResponseBody({},"There was unexpected error",500)
+    itinerary=response.json()
+    return ResponseBody({"itinerary":itinerary},"Trips created")
 
 #Gets all of an user by id
 @router.get("/trips/{id}", response_model=ResponseBody)
--- app/schemas/forms_schema.py
+++ app/schemas/forms_schema.py
@@ -1,25 +1,46 @@
 from datetime import datetime, timedelta
-from typing import List,Dict
+from typing import List,Dict,Any
 from pydantic import BaseModel
 from enum import Enum
 
 
+class QuestionType(Enum):
+    SCALE="scale"
+    SELECT="select"
+
 class Question(BaseModel):
-    id:str
-    value:int
-    typeQuestion:str
+    question_id:int
+    value:Any
+    type:QuestionType
+    class Config:
+        use_enum_values = True
 
 class TripType(Enum):
     PLACE= "place"
     ROAD = "road"
     ZONE = "zone"
 
+class LatLong(BaseModel):
+    latitude:float
+    longitude:float
+
+class Road(BaseModel):
+    origin:LatLong
+    destination:LatLong
+
+class Place(BaseModel):
+    coordinates:LatLong
+
+class Zone(BaseModel):
+    coordinates:LatLong
+
 class Form(BaseModel):
-    budget:int
+    budget:float
     dateStart:datetime
     duration:int # duration in days
-    tripType:TripType
-    people:List[str]
-    user_questions:Dict[str,List[Question]]
+    tripType:str
+    place:Place | Zone | Road
+    users:List[str]
+    questions:Dict[str,List[Question]]
 
 
