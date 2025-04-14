64ed7c629d69e55f50acff5be17bf88eddb80857
guilherme096 <guilherme.rosa60@gmail.com>
2025-04-14 22:00:02 +0100

    Adapt for radius calculations

--- app/schemas/forms_schema.py
+++ app/schemas/forms_schema.py
@@ -1,46 +1,53 @@
 from datetime import datetime, timedelta
-from typing import List,Dict,Any
+from typing import List, Dict, Any
 from pydantic import BaseModel
 from enum import Enum
 
 
 class QuestionType(Enum):
-    SCALE="scale"
-    SELECT="select"
+    SCALE = "scale"
+    SELECT = "select"
+
 
 class Question(BaseModel):
-    question_id:int
-    value:Any
-    type:QuestionType
+    question_id: int
+    value: Any
+    type: QuestionType
+
     class Config:
         use_enum_values = True
 
+
 class TripType(Enum):
-    PLACE= "place"
+    PLACE = "place"
     ROAD = "road"
     ZONE = "zone"
 
+
 class LatLong(BaseModel):
-    latitude:float
-    longitude:float
+    latitude: float
+    longitude: float
+
 
 class Road(BaseModel):
-    origin:LatLong
-    destination:LatLong
+    origin: LatLong
+    destination: LatLong
+
 
 class Place(BaseModel):
-    coordinates:LatLong
+    coordinates: LatLong
+    place_name: str
+
 
 class Zone(BaseModel):
-    coordinates:LatLong
+    coordinates: LatLong
+
 
 class Form(BaseModel):
-    budget:float
-    dateStart:datetime
-    duration:int # duration in days
-    tripType:str
-    place:Place | Zone | Road
-    users:List[str]
-    questions:Dict[str,List[Question]]
-
-
+    budget: float
+    dateStart: datetime
+    duration: int  # duration in days
+    tripType: str
+    place: Place | Zone | Road
+    users: List[str]
+    questions: Dict[str, List[Question]]
