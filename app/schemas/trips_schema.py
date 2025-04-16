7a6f79585e8ea721c06d31719e8829202469aa97
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2025-04-16 21:21:14 +0100

    create, save and get trips endpoints with generate ID and cache attachment

--- app/schemas/trips_schema.py
+++ app/schemas/trips_schema.py
@@ -1,26 +1,57 @@
 from datetime import datetime, timedelta
-from typing import TypedDict,List,Tuple
+from typing import List,Optional,Dict
+from pydantic import BaseModel,field_validator
 from enum import Enum
 # Modified classes for MongoDB compatibility
 
-class Place(TypedDict):
-    placeId: str
-    location: Tuple[str, str]
-    start_time: datetime
-    end_time: datetime
+class LatLong(BaseModel):
+    latitude: float
+    longitude: float
 
-class Route(TypedDict):
-    polylines: List[str]
-    duration: datetime
+class PlaceInfo(BaseModel):
+    id: Optional[str] = None
+    name: str
+    location: LatLong
+    types: List[str]
+    photos: Optional[List] = None
+    accessibility_options: Optional[Dict] = None
+    opening_hours: Optional[Dict] = None
+    price_range: Optional[str] = None
+    rating: Optional[float] = None
+    user_ratings_total: Optional[int] = None
+    international_phone_number: Optional[str] = None
+    national_phone_number: Optional[str] = None
+    allows_dogs: Optional[bool] = None
+    good_for_children: Optional[bool] = None
+    good_for_groups: Optional[bool] = None
 
-class Day(TypedDict):
-    template_type: str
-    places: List[Place]
-    routes: List[Route]
 
-class Trip(TypedDict):
-    image: str
-    days: List[Day]
-    state:str
-    people: List[str]
+class Activity(BaseModel):
+    place: PlaceInfo
+    start_time: datetime | str
+    end_time: datetime | str
+    activity_type: str
+    duration: int  # in minutes
 
+class Route(BaseModel):
+    polylineEncoded: str
+    duration: int
+    distance: int
+
+
+class Day(BaseModel):
+    date: datetime | str
+    morning_activities: List[Activity] = []
+    afternoon_activities: List[Activity] = []
+    routes: Optional[List[Route]] = None
+
+
+class Trip(BaseModel):
+    start_date: datetime | str
+    end_date: datetime | str
+    days: List[Day] = []
+
+
+class TripSaveRequest(BaseModel):
+    id: str 
+    itinerary: Trip
